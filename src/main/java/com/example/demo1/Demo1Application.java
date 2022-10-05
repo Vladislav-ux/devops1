c65cf1c231cbf12f4d0ee7e835d5982e19005b6a
vladislav-ux <vladik201518@gmail.com>
2022-10-05 13:56:55 +0300

    init commit

---
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -0,0 +1,21 @@
+package com.example.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Demo1Application {
+
+    @GetMapping("/hello")
+    public String message() {
+        return "Hello, world!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1Application.class, args);
+    }
+
+}
